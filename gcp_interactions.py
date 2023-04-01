1943c4e928c078290075a04e5c7b7dc64eb69e4b
AlexandreGarito <alexandre.garito@gmail.com>
2023-04-01 19:52:12 +0200

    Reordered gcp_interactions module

--- gcp_interactions.py
+++ gcp_interactions.py
@@ -1,6 +1,10 @@
 """Module for all the interactions with GCP for the project"""
 
 
+import logging
+import os
+from google.cloud.sql.connector import Connector
+import sqlalchemy
 from google.cloud import secretmanager
 
 
@@ -24,3 +28,51 @@ def get_secret(project_id, secret_name):
     response = client.access_secret_version(name=path_secret_name)
     secret_value = response.payload.data.decode("UTF-8")
     return secret_value
+
+
+PROJECT_ID = os.environ["PROJECT_ID"] # same as demo1
+SQL_INSTANCE_CONNECTION_NAME1 = get_secret(PROJECT_ID, "SQL_INSTANCE_CONNECTION_NAME1") # same as demo1
+SQL_DB_USER1 = get_secret(PROJECT_ID, "SQL_DB_USER1")
+SQL_DB_PASS1 = get_secret(PROJECT_ID, "SQL_DB_PASS1")
+SQL_DB_NAME2 = get_secret(PROJECT_ID, "SQL_DB_NAME2")
+
+
+def conn_to_psql():
+    """Connects to the GCP Cloud SQL PostgreSQL database"""
+    
+    logging.info("Connecting to database...")
+    
+    connector = Connector()
+
+    def getconn_SQL():
+        conn = connector.connect(
+            SQL_INSTANCE_CONNECTION_NAME1,
+            "pg8000",
+            user=SQL_DB_USER1,
+            password=SQL_DB_PASS1,
+            db=SQL_DB_NAME2,
+        )
+        return conn
+
+    # create connection pool with 'creator' argument to our connection object
+    try:
+        pool = sqlalchemy.create_engine(
+            "postgresql+pg8000://",
+            creator=getconn_SQL,
+        )
+        logging.info("Connection successfull!")
+    except Exception as e:
+        logging.warning(f"Connection to GCP database failed!\nThis exception was raised : {e}")
+        
+    return pool, connector
+
+
+def close_conn_to_sql(pool, connector):
+    """Closes the connection to the GCP Cloud SQL PostgreSQL database"""
+    
+    logging.info("Closing connection to database...")
+    
+    connector.close() # clean up the Connector object only used to authenticate the user
+    pool.dispose() # close the database connections managed by the connection pool
+    
+    logging.info("Connection closed.")
