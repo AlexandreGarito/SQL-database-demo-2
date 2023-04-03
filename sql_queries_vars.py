47251333abc70536d4aa65584baf731b65b0b9ba
AlexandreGarito <alexandre.garito@gmail.com>
2023-04-03 02:33:46 +0200

    Updated SQL queries camelCase to lowercase

--- sql_queries_vars.py
+++ sql_queries_vars.py
@@ -6,26 +6,26 @@
 create_table_glassdoor = '''
     CREATE TABLE glassdoor (
         id SERIAL PRIMARY KEY,
-        gaTrackerData_jobTitle VARCHAR,
-        gaTrackerData_location VARCHAR,
-        gaTrackerData_locationType VARCHAR,
-        gaTrackerData_sector VARCHAR,
-        header_easyApply BOOLEAN,
-        header_employerName VARCHAR,
-        header_jobTitle VARCHAR,
+        gaTrackerdata_jobtitle VARCHAR,
+        gaTrackerdata_location VARCHAR,
+        gaTrackerdata_locationtype VARCHAR,
+        gaTrackerdata_sector VARCHAR,
+        header_easyapply BOOLEAN,
+        header_employername VARCHAR,
+        header_jobtitle VARCHAR,
         header_location VARCHAR,
         header_posted VARCHAR,
-        header_rating REAL,
-        header_urgencyLabel VARCHAR,
-        header_payHigh INTEGER,
-        header_payMed INTEGER,
-        header_payLow INTEGER,
+        header_rating FLOAT,
+        header_urgencylabel VARCHAR,
+        header_payhigh INTEGER,
+        header_paymed INTEGER,
+        header_paylow INTEGER,
         job_description VARCHAR,
-        job_jobSource VARCHAR,
+        job_jobsource VARCHAR,
         map_country VARCHAR,
-        map_employerName VARCHAR,
-        map_lat REAL,
-        map_lng REAL,
+        map_employername VARCHAR,
+        map_lat FLOAT,
+        map_lng FLOAT,
         map_location VARCHAR,
         overview_foundedYear INTEGER,
         overview_hq VARCHAR,
@@ -39,13 +39,13 @@ create_table_glassdoor = '''
         overview_mission VARCHAR,
         overview_competitors INTEGER,
         rating_ceo_name VARCHAR,
-        rating_ceoApproval REAL,
-        rating_recommendToFriend REAL,
-        rating_starRating REAL,
+        rating_ceoapproval FLOAT,
+        rating_recommendtofriend FLOAT,
+        rating_starrating FLOAT,
         benefits_comments INTEGER,
         benefits_highlights INTEGER,
         reviews INTEGER,
-        salary_salaries REAL,
+        salary_salaries INTEGER,
         wwfu INTEGER,
         FOREIGN KEY (overview_competitors) REFERENCES glassdoor_overview_competitors(id),
         FOREIGN KEY (benefits_comments) REFERENCES glassdoor_benefits_comments(id),
@@ -74,9 +74,9 @@ create_table_glassdoor_bc = '''
         index INTEGER,
         benefits_comments_val_city VARCHAR,
         benefits_comments_val_comment VARCHAR,
-        benefits_comments_val_createDate VARCHAR,
-        benefits_comments_val_currentJob VARCHAR,
-        benefits_comments_val_jobTitle VARCHAR,
+        benefits_comments_val_createdate VARCHAR,
+        benefits_comments_val_currentjob VARCHAR,
+        benefits_comments_val_jobtitle VARCHAR,
         benefits_comments_val_rating INTEGER,
         benefits_comments_val_state VARCHAR
     );
@@ -87,11 +87,11 @@ create_table_glassdoor_bc = '''
 create_table_glassdoor_bh = '''
     CREATE TABLE glassdoor_benefits_highlights (
         id INTEGER PRIMARY KEY,
-        benefits_highlights_val_highlightPhrase VARCHAR,
+        benefits_highlights_val_highlightphrase VARCHAR,
         benefits_highlights_val_icon VARCHAR,
         benefits_highlights_val_name VARCHAR,
         index INTEGER,
-        benefits_highlights_val_commentCount INTEGER
+        benefits_highlights_val_commentcount INTEGER
     );
     '''
 
@@ -104,46 +104,29 @@ create_table_glassdoor_r = '''
         reviews_val_cons VARCHAR,
         reviews_val_date VARCHAR,
         reviews_val_featured VARCHAR,
-        reviews_val_helpfulCount INTEGER,
+        reviews_val_helpfulcount INTEGER,
         reviews_val_id INTEGER,
         reviews_val_pros VARCHAR,
-        reviews_val_publishedOn VARCHAR,
+        reviews_val_publishedon VARCHAR,
         reviews_val_publisher VARCHAR,
-        reviews_val_reviewRatings_careerOpportunities REAL,
-        reviews_val_reviewRatings_compBenefits REAL,
-        reviews_val_reviewRatings_cultureValues INTEGER,
-        reviews_val_reviewRatings_overall INTEGER,
-        reviews_val_reviewRatings_seniorManagement REAL,
-        reviews_val_reviewRatings_worklifeBalance REAL,
-        reviews_val_reviewerDuration VARCHAR,
-        reviews_val_reviewerInformation VARCHAR,
-        reviews_val_reviewerJobTitle VARCHAR,
-        reviews_val_reviewerLocation VARCHAR,
-        reviews_val_reviewerStatus VARCHAR,
-        reviews_val_summaryPoints_ceoApproval INTEGER,
-        reviews_val_summaryPoints_outlook INTEGER,
-        reviews_val_summaryPoints_recommend INTEGER,
+        reviews_val_reviewratings_careeropportunities FLOAT,
+        reviews_val_reviewratings_compbenefits FLOAT,
+        reviews_val_reviewratings_culturevalues INTEGER,
+        reviews_val_reviewratings_overall INTEGER,
+        reviews_val_reviewratings_seniormanagement FLOAT,
+        reviews_val_reviewratings_worklifebalance FLOAT,
+        reviews_val_reviewerduration VARCHAR,
+        reviews_val_reviewerinformation VARCHAR,
+        reviews_val_reviewerjobtitle VARCHAR,
+        reviews_val_reviewerlocation VARCHAR,
+        reviews_val_reviewerstatus VARCHAR,
+        reviews_val_summarypoints_ceoapproval INTEGER,
+        reviews_val_summarypoints_outlook INTEGER,
+        reviews_val_summarypoints_recommend INTEGER,
         reviews_val_title VARCHAR,
-        reviews_val_adviceToManagement VARCHAR,
-        reviews_val_companyResponse VARCHAR,
-        FOREIGN KEY (reviews_val_reviewResponses) REFERENCES glassdoor_reviews_val_reviewResponses(id),
-    );
-    '''
-
-
-
-create_table_glassdoor_rvr = '''
-    CREATE TABLE glassdoor_reviews_val_reviewResponses (
-        id INTEGER PRIMARY KEY,
-        index INTEGER,
-        reviews_val_reviewResponses_val_createDate VARCHAR,
-        reviews_val_reviewResponses_val_helpfulCount INTEGER,
-        reviews_val_reviewResponses_val_jobTitle VARCHAR,
-        reviews_val_reviewResponses_val_notHelpfulCount INTEGER,
-        reviews_val_reviewResponses_val_responseText VARCHAR,
-        reviews_val_reviewResponses_val_responseTextLength INTEGER,
-        reviews_val_reviewResponses_val_totalHelpfulCount INTEGER,
-        reviews_val_reviewResponses_val_updateDate VARCHAR
+        reviews_val_advicetomanagement VARCHAR,
+        reviews_val_companyresponse VARCHAR,
+        reviews_val_reviewresponses INTEGER
     );
     '''
 
@@ -153,13 +136,13 @@ create_table_glassdoor_ss = '''
 CREATE TABLE glassdoor_salary_salaries (
     id INTEGER PRIMARY KEY,
     index INTEGER,
-    salary_salaries_val_basePayCount INTEGER,
-    salary_salaries_val_jobTitle VARCHAR,
-    salary_salaries_val_payPeriod VARCHAR,
-    salary_salaries_val_salaryPercentileMap_payPercentile10 REAL,
-    salary_salaries_val_salaryPercentileMap_payPercentile90 REAL,
-    salary_salaries_val_salaryPercentileMap_payPercentile50 REAL,
-    salary_salaries_val_salaryType VARCHAR
+    salary_salaries_val_basepaycount INTEGER,
+    salary_salaries_val_jobtitle VARCHAR,
+    salary_salaries_val_payperiod VARCHAR,
+    salary_salaries_val_salarypercentilemap_paypercentile10 FLOAT,
+    salary_salaries_val_salarypercentilemap_paypercentile90 FLOAT,
+    salary_salaries_val_salarypercentilemap_paypercentile50 FLOAT,
+    salary_salaries_val_salarytype VARCHAR
 );
 '''
 
@@ -172,6 +155,9 @@ create_table_glassdoor_w = '''
         wwfu_val_id INTEGER,
         wwfu_val_title VARCHAR,
         wwfu_val_type VARCHAR,
+        wwfu_val_videos INTEGER,
+        wwfu_val_photos INTEGER,
+        wwfu_val_captions INTEGER,
         FOREIGN KEY (wwfu_val_videos) REFERENCES glassdoor_wwfu_val_videos(id),
         FOREIGN KEY (wwfu_val_photos) REFERENCES glassdoor_wwfu_val_photos(id),
         FOREIGN KEY (wwfu_val_captions) REFERENCES glassdoor_wwfu_val_captions(id)
@@ -184,7 +170,7 @@ create_table_glassdoor_wvc = '''
     CREATE TABLE glassdoor_wwfu_val_captions (
         id INTEGER PRIMARY KEY,
         index INTEGER,
-        wwfuf_val_captions_val VARCHAR
+        wwfu_val_captions_val VARCHAR
     );
     '''
 
@@ -194,7 +180,7 @@ create_table_glassdoor_wvp = '''
     CREATE TABLE glassdoor_wwfu_val_photos (
         id INTEGER PRIMARY KEY,
         index INTEGER,
-        wwfuf_val_photos_val VARCHAR
+        wwfu_val_photos_val VARCHAR
     );
     '''
 
@@ -204,7 +190,7 @@ create_table_glassdoor_wvv = '''
     CREATE TABLE glassdoor_wwfu_val_videos (
         id INTEGER PRIMARY KEY,
         index INTEGER,
-        wwfuf_val_videos_val VARCHAR
+        wwfu_val_videos_val VARCHAR
     );
     '''
 
